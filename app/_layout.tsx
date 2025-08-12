6145369dd52d49d673bba177a0fa0f53cc9ef5a4
Yurii Synkulych <yurii.s@code-care.pro>
2025-08-12 08:04:18 +0300

    feat: added safe area paddings and chat transcript to the screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
+import { StyleSheet } from "react-native";
 import { Stack } from "expo-router";
+import {
+  SafeAreaProvider,
+  SafeAreaView,
+  initialWindowMetrics,
+} from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <SafeAreaView style={styles.container}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
